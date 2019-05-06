3c3092cffef8b533fe6cb0246b8049a3a5880e25
Ruben <rubenjohne@gmail.com>
2019-05-06 22:19:30 +0000

    part 1/2 of user admin update

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190430222637) do
+ActiveRecord::Schema.define(version: 20190503221911) do
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20190430222637) do
     t.datetime "updated_at",      null: false
     t.string   "password_digest"
     t.string   "remember_digest"
+    t.boolean  "admin"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
