a74fd5940d6ca7a6501d8417c0186e750cdab770
Ruben <rubenjohne@gmail.com>
2019-09-25 00:14:37 +0000

    created micropost list

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -1,6 +1,11 @@
 class MicropostsController < ApplicationController
-    before_action :logged_in_user, only:[:create, :destroy]
+    before_action :logged_in_user, only:[:index, :create, :destroy]
     before_action :correct_user, only: :destroy
+    before_action :admin_user, only: [:index, :destroy ]  
+    
+    def index
+        @microposts = Micropost.all
+    end
     
     def create
         @micropost = current_user.microposts.build(micropost_params)
@@ -29,4 +34,9 @@ class MicropostsController < ApplicationController
             @micropost = current_user.microposts.find_by(id: params[:id])
             redirect_to root_url if @micropost.nil?
         end
+        
+        def admin_user
+            redirect_to(root_url)  unless current_user.admin?
+        end
+        
 end
